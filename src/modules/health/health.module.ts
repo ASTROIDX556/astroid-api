9de2ebc571b279a8b4e9cf69d009ef9bbf34072d
Teescom <adebisiallison2018@gmail.com>
2026-08-28 14:12:44 +0100

    feat: add/update src/modules/health/health.module.ts

---
+++ src/modules/health/health.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { StellarHealthIndicator } from './indicators/stellar.health';
+import { HealthController } from './health.controller';
+
+@Module({
+  controllers: [HealthController],
+  providers: [StellarHealthIndicator],
+  exports: [StellarHealthIndicator],
+})
+export class HealthModule {}
