bc3aefd793866505cdc727c22a1f360e0c7ab0a7
Ayo_Dev <amireayodele2002@gmail.com>
2026-09-01 11:09:37 +0100

    fix(ci): resolve failing checks for #18

--- src/config/stellar.config.ts
+++ src/config/stellar.config.ts
@@ -1,4 +1,4 @@
-import { registerAs } from 'nestjs/config';
+import { registerAs } from '@nestjs/config';
 import { stellarEnvSchema, validateEnv } from './env.validation';
 
 export type StellarConfig = {
