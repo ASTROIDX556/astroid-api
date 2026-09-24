664534a729797588801991e635c91d593ded7b63
precious1joe <162345921+precious1joe@users.noreply.github.com>
2026-09-24 09:05:43 +0100

    Fix issue #65: update src/modules/health/index.ts

--- src/modules/health/index.ts
+++ src/modules/health/index.ts
@@ -1,4 +1,6 @@
 export * from './health.module';
 export * from './health.controller';
+export * from './indicators/database-connection.health';
+export * from './indicators/redis.health';
 export * from './indicators/stellar.health';
 export * from './indicators/database-migration.health';
